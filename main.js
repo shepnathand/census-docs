08ef8226f1d22aab8dfa769900a5327882319ebc
Nathan Sheppard <shep.nathan.d@gmail.com>
2018-07-12 14:44:02 -0400

    added simple tag filtering

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 $(document).ready(function() {
-    var values = [];
     $('div.tag-container').children().click('change', function() {
         var elements = $('div.posts').children().hide(); // hide all the elements
-        values.push($(this).text());
+        var value = ($(this).text());
 
-        if (values.length > 0) { // if somethings' selected
-            for ( index in values ) {
-                var value = values[index];
-                elements.filter('.' + value).show(); // show the ones we want
-            };
-            var tag = '<p><span id="tag" value="' + value + '">' + value + '</span></p>';
-
-            if ( $('div.active-tags p span').text().indexOf( value ) == -1 ) {
-                $('div.active-tags').append(tag);
-            };
+        if (value.length > 0) { // if somethings' selected
+            elements.filter('.' + value).show(); // show the ones we want
         }
     }).trigger('change');
 
     $('div.tag-container span').hover(function() {
-        $(this).css("cursor", "pointer");
         $(this).css("background-color", "#efefef");
     },function () {
         $(this).css("background-color", "#dfdfdf");
